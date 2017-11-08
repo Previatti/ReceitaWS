fb8be6bdc936248a5a6ee2dbfea4567ee4a0b60a
fabitosam <fabio.rezende@gmail.com>
2017-11-08 16:27:02 -0300

    Initial commit

---
+++ WebApplication2/Controllers/BaseController.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class BaseController : Controller
+    {
+        public async Task<T> GetWSObject<T>(string uriActionString)
+        {
+            T returnValue =
+                default(T);
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(@"https://www.receitaws.com.br/");
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpResponseMessage response = await client.GetAsync(uriActionString);
+                    response.EnsureSuccessStatusCode();
+                    returnValue = JsonConvert.DeserializeObject<T>(((HttpResponseMessage)response).Content.ReadAsStringAsync().Result);
+                }
+                return returnValue;
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+        }
+    }
+}
---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        HttpClient client;
+        //The URL of the WEB API Service
+        string url = "https://www.receitaws.com.br/v1/cnpj/";
+
+        //The HttpClient Class, this will be used for performing 
+        //HTTP Operations, GET, POST, PUT, DELETE
+        //Set the base address and the Header Formatter
+        public HomeController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<ActionResult> Index(string id)
+        {
+            HttpResponseMessage responseMessage = await client.GetAsync(url + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+
+                var Company = JsonConvert.DeserializeObject<Company>(responseData);
+
+                return View(Company);
+            }
+            return View("Error");
+        }       
+
+    }
+}
